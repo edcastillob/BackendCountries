7848e08eab07f75dec2c3ff77d0972daf6e7cbbe
edcastillob <edwar.castillo@gmail.com>
2023-07-04 20:48:40 -0400

    Credenciales

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
-const server = require("./src/server");
-const { conn } = require('./src/db.js');
-// const fCargarDB = require('./src/DB_json-Postgres/fCargarDB.js');
+
 const PORT =  process.env.PORT;
-// const PORT =  process.env.PORT || 3001;
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const CountryModel = require('./models/Country')
+const ActivityModel = require('./models/Activity')
 
 
+const express = require("express");
+const router = require("./routes");
+const morgan = require("morgan");
+const cors = require("cors");
+const server = express();
+
+server.use(morgan("dev"));
+server.use(express.json());
+server.use(cors());
+
+
+
+
+
+
+
+const {DB_DEPLOY } = process.env;
+const sequelize = new Sequelize( DB_DEPLOY,
+  
+  {
+  logging: false, 
+  native: false, 
+  dialectModule: pg,
+});
+
+
+CountryModel(sequelize);
+ActivityModel(sequelize);
+
+const { Country, Activity } = sequelize.models;
+
+Country.belongsToMany(Activity, {through: "CountryActivity"});
+Activity.belongsToMany(Country, {through: "CountryActivity"});
+
+
+
+
+server.use(router);
 conn.sync({ force: false })
 .then(() => {
   // fCargarDB()
