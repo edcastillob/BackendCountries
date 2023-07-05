99bda025b1e5bc8945f178c2bda84497bb2e470c
edcastillob <edwar.castillo@gmail.com>
2023-07-04 20:51:08 -0400

    Credenciales

--- index.js
+++ index.js
@@ -1,52 +1,12 @@
-
-const PORT =  process.env.PORT;
-require("dotenv").config();
-const { Sequelize } = require("sequelize");
-const CountryModel = require('./models/Country')
-const ActivityModel = require('./models/Activity')
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const fCargarDB = require('./src/DB_json-Postgres/fCargarDB.js');
+const PORT = 3001 || process.env.PORT;
 
 
-const express = require("express");
-const router = require("./routes");
-const morgan = require("morgan");
-const cors = require("cors");
-const server = express();
-
-server.use(morgan("dev"));
-server.use(express.json());
-server.use(cors());
-
-
-
-
-
-
-
-const {DB_DEPLOY } = process.env;
-const sequelize = new Sequelize( DB_DEPLOY,
-  
-  {
-  logging: false, 
-  native: false, 
-  dialectModule: pg,
-});
-
-
-CountryModel(sequelize);
-ActivityModel(sequelize);
-
-const { Country, Activity } = sequelize.models;
-
-Country.belongsToMany(Activity, {through: "CountryActivity"});
-Activity.belongsToMany(Country, {through: "CountryActivity"});
-
-
-
-
-server.use(router);
-conn.sync({ force: false })
+conn.sync({ force: true })
 .then(() => {
-  // fCargarDB()
+  fCargarDB()
   server.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 })
